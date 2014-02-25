e8f70e5ddca89188f8fc52395ea5c4c0c4ad27ae
clardeur101413 <clement.lardeur-ext@sgcib.com>
2014-02-25 19:39:36 +0100

    remove: cluster folder

--- cluster/nodes/1/cassandra.in.sh
+++
@@ -1,9 +0,0 @@
-#Self-Contained inti, assumes that cassandra is at /home/notroot/cassandra
-echo 'setting cassandra_home'
-CASSANDRA_CONF="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-CLASSPATH=$CASSANDRA_CONF
-
-for jar in $CASSANDRA_HOME/lib/*.jar; do
-    CLASSPATH=$CLASSPATH:$jar
-done
