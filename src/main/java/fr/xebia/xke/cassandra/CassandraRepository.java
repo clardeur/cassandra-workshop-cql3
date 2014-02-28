ccc3fdc8ba7900c00316d2a17854fc0b2b91355d
Clement Lardeur <clem.lardeur@gmail.com>
2014-02-28 21:41:57 +0100

    remove: solution

--- src/main/java/fr/xebia/xke/cassandra/CassandraRepository.java
+++ src/main/java/fr/xebia/xke/cassandra/CassandraRepository.java
@@ -1,34 +1,17 @@
 package fr.xebia.xke.cassandra;
 
-import static com.datastax.driver.core.querybuilder.QueryBuilder.batch;
-import static com.datastax.driver.core.querybuilder.QueryBuilder.eq;
-import static com.datastax.driver.core.querybuilder.QueryBuilder.gt;
-import static com.datastax.driver.core.querybuilder.QueryBuilder.insertInto;
-import static com.datastax.driver.core.querybuilder.QueryBuilder.lt;
-import static com.datastax.driver.core.querybuilder.QueryBuilder.select;
-import static com.datastax.driver.core.querybuilder.QueryBuilder.ttl;
-import static java.lang.String.format;
-
 import java.util.Date;
 import java.util.Set;
 import java.util.UUID;
 
-import fr.xebia.xke.cassandra.model.Track;
-import fr.xebia.xke.cassandra.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.datastax.driver.core.BoundStatement;
-import com.datastax.driver.core.ExecutionInfo;
-import com.datastax.driver.core.Host;
-import com.datastax.driver.core.PreparedStatement;
-import com.datastax.driver.core.QueryTrace;
-import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.ResultSetFuture;
-import com.datastax.driver.core.Session;
-import com.datastax.driver.core.Statement;
-import com.datastax.driver.core.querybuilder.Batch;
-import com.datastax.driver.core.utils.UUIDs;
+import com.datastax.driver.core.*;
+import fr.xebia.xke.cassandra.model.Track;
+import fr.xebia.xke.cassandra.model.User;
+
+import static java.lang.String.format;
 
 public class CassandraRepository {
 
@@ -46,48 +29,25 @@ public class CassandraRepository {
      * http://www.datastax.com/documentation/cql/3.0/cql/cql_reference/insert_r.html
      */
     public void insertUserWithQueryString(User user) {
-        LOG.debug("insert user: {}", user);
-        session.execute("INSERT INTO user (id, name, email, age) VALUES (?,?,?,?)",
-                user.getId(),
-                user.getName(),
-                user.getEmail(),
-                user.getAge());
     }
 
     /**
      * Save an user by using a {@link com.datastax.driver.core.querybuilder.QueryBuilder}.
      */
     public void insertUserWithQueryBuilder(User user) {
-        LOG.debug("insert user: {}", user);
-        session.execute(insertInto("user")
-                .value("id", user.getId())
-                .value("name", user.getName())
-                .value("email", user.getEmail())
-                .value("age", user.getAge()));
     }
 
     /**
      * Read a user with by using a {@link com.datastax.driver.core.querybuilder.QueryBuilder}..
      */
     public ResultSet findUserWithQueryBuilder(UUID id) {
-        return session.execute(select().from("user").where(eq("id", id)));
+        return null;
     }
 
     /**
      * Insert a track with by using a {@link com.datastax.driver.core.PreparedStatement}.
      */
     public void insertTracksWithPreparedStatement(Iterable<Track> tracks) {
-        PreparedStatement preparedStatement = session.prepare("INSERT INTO tracks (id, title, release, duration, tags) VALUES (?,?,?,?,?)");
-        for (Track track : tracks) {
-            LOG.debug("insert track: {}", track);
-            BoundStatement boundStatement = preparedStatement.bind(
-                    track.getId(),
-                    track.getTitle(),
-                    track.getRelease(),
-                    track.getDuration(),
-                    track.getTags());
-            session.execute(boundStatement);
-        }
     }
 
     /**
@@ -98,22 +58,13 @@ public class CassandraRepository {
      * @param ttl the TTL (Time To Live) in seconds
      */
     public void insertUserClickStreamWithTTL(UUID userId, Date when, String url, int ttl) {
-        LOG.debug("insert into user_click_stream [{}, {}]", userId, when.getTime());
-        session.execute(insertInto("user_click_stream")
-                .value("user_id", userId)
-                .value("when", when)
-                .value("url", url)
-                .using(ttl(ttl)));
     }
 
     /**
      * Read user click stream between two dates asynchronously.
      */
     public ResultSet findUserClicksStreamWithinTimeFrame(UUID userId, Date start, Date end) {
-        return session.execute(select().from("user_click_stream")
-                .where(eq("user_id", userId))
-                .and(gt("when", start))
-                .and(lt("when", end)));
+        return null;
     }
 
     /**
@@ -122,38 +73,27 @@ public class CassandraRepository {
      * {@link {@link com.datastax.driver.core.Statement#setFetchSize(int)}}
      */
     public ResultSet findUserClicksStreamByPage(UUID userId, int pageSize) {
-        return session.execute(select().from("user_click_stream").where(eq("user_id", userId)).setFetchSize(pageSize));
+        return null;
     }
 
     /**
      * Execute an async write query {@link com.datastax.driver.core.Session#executeAsync(Statement)}
      */
     public ResultSetFuture insertUserTrackLikesAsync(UUID userId, UUID trackId) {
-        LOG.debug("insert into track_likes [{}, {}]", userId, trackId);
-        return session.executeAsync(
-                insertInto("track_likes")
-                        .value("user_id", userId)
-                        .value("track_id", trackId)
-        );
+        return null;
     }
 
     /**
      * Use a batch query {@link com.datastax.driver.core.querybuilder.Batch}
      */
     public void insertUserTrackLikesByBatch(UUID userId, Set<Track> tracks) {
-        Batch batch = batch();
-        for (Track track : tracks) {
-            LOG.debug("insert into track_likes [{}, {}]", UUIDs.random(), track.getId());
-            batch.add(insertInto("track_likes").value("user_id", userId).value("track_id", track.getId()));
-        }
-        session.execute(batch);
     }
 
     /**
      * {@link com.datastax.driver.core.Statement#enableTracing()}
      */
     public ResultSet findUserWithTracing(UUID userId) {
-        return session.execute(select().from("user").where(eq("id", userId)).enableTracing());
+        return null;
     }
 
     /**
